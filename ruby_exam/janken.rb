2bf91fe3d400f27e47299620b661d87241d83fd8
kenya ohara <bureibu20@gmail.com>
2021-07-06 17:12:57 +0900

    fix2

--- ruby_exam/janken.rb
+++ ruby_exam/janken.rb
@@ -7,26 +7,11 @@ class Player
         # 変数「input_hand」にプレイヤーの入力値を代入します。
         # ヒント：getsメソッドについて調べてみましょう。
         input_hand = gets.chomp
-        
-        # 「input_hand」が「0, 1, 2」のいずれかだと繰り返し処理を終了し、それ以外（アルファベットも含む）だと繰り返し処理を継続します。
-        while true do
-            # if 「input_hand」が「0, 1, 2」のいずれかの場合だった場合
-            # ヒント：include?メソッドについて調べてみましょう。
-            # 「input_hand」をそのまま返す。
-            # ヒント：戻り値を返して繰り返し処理を終了させたい場合、「return」を使用します。
-            if input_hand.include?("0") || input_hand.include?("1") || input_hand.include?("2")
-                return input_hand
-            # else それ以外の場合
-            # プレイヤーに「0〜2」を入力させる文章を表示させる。
-            # puts "0〜2の数字を入力してください。"
-            # puts "0:グー, 1:チョキ, 2:パー"
-            # 変数「input_hand」にプレイヤーの入力値を代入します。
-            else
-                puts "0〜2の数字を入力してください。"
-                puts "0:グー, 1:チョキ, 2:パー"
-                input_hand =　gets.chomp
-            # end if文のend
-            end
+
+        if input_hand == "0" || input_hand == "1" || input_hand == "2"
+            return input_hand.to_i
+        else
+            self.hand
         end
     end
 end
@@ -85,9 +70,9 @@ class GameStart
         next_game = true
         # 「next_game」が「false」だと繰り返し処理を終了し、「true」だと繰り返し処理を継続します。
         while next_game 
-        # 変数「next_game」にじゃんけんを実行して返ってきた値(戻り値)を代入します。
-        #「janken.pon(player.hand, enemy.hand)」でじゃんけんを実行しています。
-        next_game = janken.pon(player.hand , enemy.hand)
+            # 変数「next_game」にじゃんけんを実行して返ってきた値(戻り値)を代入します。
+            #「janken.pon(player.hand, enemy.hand)」でじゃんけんを実行しています。
+            next_game = janken.pon(player.hand , enemy.hand)
         end
     end
 end
